b897095f6c3ade5cc924217495337db4b5b23a30
shahsawar92 <shahsawar92@gmail.com>
2021-11-10 15:07:01 +0500

    paypal integration fixed

--- src/Components/Profile/price_cards/prices.js
+++ src/Components/Profile/price_cards/prices.js
@@ -7,7 +7,7 @@ export default function Prices() {
     const [starterActive,setStarterActive]=useState(false)
     const [proActive,setproActive]=useState(false)
     const [vipActive,setvipActive]=useState(false)
-    const [counterPro, setCounterPro]=useState(0)
+    const [counterPro, setCounterPro]=useState(1)
     const [showModal, setShowModal] = useState(false);
     const [showModal2, setShowModal2] =useState(false);
     const [showModal3, setShowModal3] = useState(false);
@@ -23,36 +23,40 @@ export default function Prices() {
   let proPlan = false;
   let vipPlan = true;
   useEffect(()=>{
+      if(starterPlan||proPlan||vipPlan){
       if(starterPlan){ setStarterActive(true); setvipActive(false);setproActive(false)}
       if (proPlan ) { setStarterActive(false); setvipActive(false);setproActive(true)}
       if (vipPlan )  { setStarterActive(false); setvipActive(true);setproActive(false)}
-  } ,[])
+      }else{
+         setStarterActive(false); setvipActive(false);setproActive(false)
+      }
+  } ,[starterPlan, proPlan, vipPlan])
   
-    
+console.log("proactive, vipactive, starteractive",proActive,vipActive,starterActive);
   //functions
     const handleClick=(e,props)=>{
-        console.log(e.target.innerText);
+        console.log(e?.target?.innerText);
         if(props===1){
             
             setShowModal(true);
         }
         if(props===2){
             
-            if(e.target.innerText=="Add Grooves"){
+            if(e?.target?.innerText=="Add Grooves"){
                 setaddProGroves(true);
             }else
             setShowModal2(true);
         }
         if(props===3){
-            if(e.target.innerText=="Add Grooves"){
+            if(e?.target?.innerText=="Add Grooves"){
                 setaddVipGrooves(true);
             }else
             setShowModal3(true);
         }
     }
     const handleCounterminus=()=>{
-            if(counterVip<=0){
-                setCounterVip(0)
+            if(counterVip<=1){
+                setCounterVip(1)
             }else 
             setCounterVip(counterVip-1);
         
@@ -66,8 +70,8 @@ export default function Prices() {
    
     }
     const handleCounterminusPro=()=>{
-        if(counterPro<=0){
-            setCounterPro(0)
+        if(counterPro<=1){
+            setCounterPro(1)
         }else 
         setCounterPro(counterPro-1);
     
@@ -90,8 +94,7 @@ const handleCounterplusPro=()=>{
         <div className="flex md:flex-row flex-col md:justify-evenly ">
     
         
-           <div  className={starterActive ? "md:w-1/4 w-7/12  self-center my-6 bg-white shadow-xl border-2 border-red-600 ":"md:w-1/4 w-7/12 opacity-40 self-center my-6 bg-white shadow-xl border-t-2"}
-        >
+        <div  className={starterActive? "md:w-1/4 w-7/12  self-center my-6 bg-white shadow-xl border-2 border-red-600 ":(!vipActive && !proActive && !starterActive)?"md:w-1/4 w-7/12  self-center my-6 bg-white shadow-xl border-2 ":"md:w-1/4 w-7/12 opacity-40 self-center my-6 bg-white shadow-xl border-t-2"}>
            <div className="flex flex-col py-4">
            <h1 className="self-center  text-xl">STARTER</h1>
                         <hr/>
@@ -101,8 +104,8 @@ const handleCounterplusPro=()=>{
                         <p className="text-lg font-semibold self-center pt-2 line-through ">Customization : x </p>
                         <h2 className="text-3xl font-semibold self-center pb-2 pt-4 ">$15</h2>
                         <button className=" w-4/5  border-2 py-2 shadow-lg self-center rounded-lg hover:bg-gray-500"
-                          onClick={()=>handleClick(1)}
-                          disabled={vipActive || proActive} 
+                          onClick={(e)=>handleClick(e,1)}
+                          disabled={proActive || vipActive } 
                         >Subscribe</button>
                    </div>
 
@@ -111,7 +114,7 @@ const handleCounterplusPro=()=>{
             {/* end of first */}
             {/* 2nd */}
            
-            <div  className={proActive ? "md:w-1/4 w-7/12  self-center my-6 bg-white shadow-xl border-2 border-red-600 ":"md:w-1/4 w-7/12 opacity-40 self-center my-6 bg-white shadow-xl border-t-2"}>
+            <div  className={proActive? "md:w-1/4 w-7/12  self-center my-6 bg-white shadow-xl border-2 border-red-600 ":(!vipActive && !proActive && !starterActive)?"md:w-1/4 w-7/12  self-center my-6 bg-white shadow-xl border-2 ":"md:w-1/4 w-7/12 opacity-40 self-center my-6 bg-white shadow-xl border-t-2"}>
            <div className="flex flex-col py-4">
 
            <h1 className="self-center text-xl">PRO</h1>
@@ -121,9 +124,12 @@ const handleCounterplusPro=()=>{
                         <p className="text-lg font-semibold self-center pt-3 ">Extra Grooves : 80</p>
                         <p className="text-lg font-semibold self-center pt-2 line-through ">Customization : x </p>
                         <h2 className="text-3xl font-semibold self-center pb-2 pt-2 ">$500</h2>
-                        <span className={vipActive||starterActive?"hidden": "self-center"} id={"VipCounter"} ><button className="py-2 px-2 bg-gray-400 " onClick={()=>handleCounterminusPro()}>-</button><span className="border-2 border-gray-800 py-2 px-4">{counterPro}</span> <button className="py-2 px-2 bg-gray-400 " onClick={()=>handleCounterplusPro()}>+</button></span>
+
+                        <span className={proActive?"self-center": "hidden"}
+                        
+                        id={"VipCounter"} ><button className="py-2 px-2 bg-gray-400 " onClick={()=>handleCounterminusPro()}>-</button><span className="border-2 border-gray-800 py-2 px-4">{counterPro}</span> <button className="py-2 px-2 bg-gray-400 " onClick={()=>handleCounterplusPro()}>+</button></span>
                         <button className="border-2 w-4/5 py-2 shadow-lg self-center rounded-lg hover:bg-gray-500 "
-                        disabled={starterActive || vipActive}  
+                        disabled={starterActive || vipActive }  
                         onClick={(e)=>handleClick(e,2)}
                         >{proActive?"Add Grooves":"Subscribe"}</button>
                    </div>
@@ -133,16 +139,19 @@ const handleCounterplusPro=()=>{
             {/* end of second */}
             {/* start of 3rd */}
            
-            <div  className={vipActive ? "md:w-1/4 w-7/12  self-center my-6 bg-white shadow-xl border-2 border-red-600 ":"md:w-1/4 w-7/12 opacity-40 self-center my-6 bg-white shadow-xl border-t-2"}>
+            <div  className={vipActive? "md:w-1/4 w-7/12  self-center my-6 bg-white shadow-xl border-2 border-red-600 ":(!vipActive && !proActive && !starterActive)?"md:w-1/4 w-7/12  self-center my-6 bg-white shadow-xl border-2 ":"md:w-1/4 w-7/12 opacity-40 self-center my-6 bg-white shadow-xl border-t-2"}>
            <div className="flex flex-col py-4">
            <h1 className="self-center text-xl">VIP</h1>
                         <hr/>
                         <p className="text-sm self-center pt-4"> Build your own and more</p>
                         <p className=" text-lg font-semibold self-center pt-4 ">Grooves : 14**</p>
-                        <p className="text-lg font-semibold self-center pt-3 ">Extra Grooves : $79</p>
+                        <p className="text-lg font-semibold self-center pt-3 ">Extra Grooves : $70</p>
                         <p className="text-lg font-semibold self-center pt-2 ">Customization : </p>
                         <h2 className="text-3xl font-semibold self-center pb-2 pt-4 ">$1000</h2>
-                        <span  className={proActive||starterActive?"hidden": "self-center"} id={"VipCounter"} ><button className="py-2 px-2 bg-gray-400 " onClick={()=>handleCounterminus()}>-</button><span className="border-2 border-gray-800 py-2 px-4">{counterVip}</span> <button className="py-2 px-2 bg-gray-400 " onClick={()=>handleCounterplus()}>+</button></span>
+
+                        <span className={vipActive?"self-center": "hidden"}
+                        
+                        id={"VipCounter"} ><button className="py-2 px-2 bg-gray-400 " onClick={()=>handleCounterminus()}>-</button><span className="border-2 border-gray-800 py-2 px-4">{counterVip}</span> <button className="py-2 px-2 bg-gray-400 " onClick={()=>handleCounterplus()}>+</button></span>
                         <button disabled={starterActive || proActive} className=" w-4/5 self-center border-2 shadow-lg rounded-lg hover:bg-gray-600 py-2"
                          onClick={(e)=>handleClick(e,3)}
                         >{vipActive?"Add Grooves":"Subscribe"}</button>
